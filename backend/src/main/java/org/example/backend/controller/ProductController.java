88750e4bf247cd5e4c11ec09d8f13cc1713d27dc
malek.b. <malek.boulakbech@gmail.com>
2025-10-06 14:56:03 +0200

    add repo

--- backend/src/main/java/org/example/backend/controller/ProductController.java
+++ backend/src/main/java/org/example/backend/controller/ProductController.java
@@ -1,6 +1,9 @@
 package org.example.backend.controller;
 
+import lombok.AllArgsConstructor;
 import org.example.backend.model.Product;
+import org.example.backend.repository.ProductRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +11,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
+@AllArgsConstructor
 public class ProductController {
 
-    public List<Product> gelAll() {
-        return null;
+    private final ProductRepository productRepository;
+
+    @GetMapping
+    public List<Product> getAll() {
+        return productRepository.findAll();
     }
 }
