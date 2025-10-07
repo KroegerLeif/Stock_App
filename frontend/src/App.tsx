c1310fdefd943c31042f1405b6603a27b12686ad
KroegerLeif <Leif.kr@outlook.com>
2025-10-07 15:14:21 +0200

    Calls a ProductPage card

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,25 @@ import './App.css'
 import {Route, Routes} from "react-router-dom";
 import ViewAllProducts from "./ViewAllProducts.tsx";
 import AddProduct from "./AddProduct.tsx";
+import DetailProductPage from "./component/DetailProductPage.tsx";
+import {useState} from "react";
+
 function App() {
 
+    const [uniqueProduct, setuniqueProduct] = useState({
+        name: "",
+        description: "",
+        price: 0,
+    });
 
   return (
     <>
         <Routes>
             <Route path={"/"} element={<ViewAllProducts/>}/>
             <Route path={"/add"} element={<AddProduct/>}/>
+            <Route path={"/products/:id"} element={<DetailProductPage productName={uniqueProduct.name}
+                                                                      description={uniqueProduct.description}
+                                                                      productPrice={uniqueProduct.price}/>}/>
         </Routes>
     </>
   )
