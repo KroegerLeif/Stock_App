278e71b6045cc093ff1c0d5a2d54b7e8ad0c5973
malek.b. <malek.boulakbech@gmail.com>
2025-10-07 12:23:55 +0200

    add button

--- frontend/src/ViewAllProducts.tsx
+++ frontend/src/ViewAllProducts.tsx
@@ -1,11 +1,13 @@
 import {useEffect, useState} from "react";
-import  axios from "axios";
+import axios from "axios";
 import type {Product} from "./Product.ts";
+import {useNavigate} from "react-router-dom";
 
-export default function ViewAllProducts(){
+export default function ViewAllProducts() {
 
     const [productsList, setProductsList] = useState<Product[]>([]);
 
+    const navigate = useNavigate();
 
     useEffect(() => {
         axios.get("/api/product")
@@ -14,20 +16,23 @@ export default function ViewAllProducts(){
     }, []);
 
     return (
-        <div className="container">
-            <h1>Produkte Liste</h1>
-            {productsList.length === 0 ? (
-                <p>Keine Produkte gefunden.</p>
-            ) : (
-                productsList.map((char) => (
-                    <div key={char.id} className="todo-item">
-                        <h3>ID: {char.id}</h3>
-                        <p><strong>Name:</strong> {char.name}</p>
-                        <p><strong>Description:</strong> {char.description}</p>
-                        <p><strong>price:</strong> {char.price}</p>
-                    </div>
-                ))
-            )}
-        </div>
+        <>
+            <button onClick={() => navigate("/products/add")}>Produkt anlegen</button>
+            <div className="container">
+                <h1>Produkte Liste</h1>
+                {productsList.length === 0 ? (
+                    <p>Keine Produkte gefunden.</p>
+                ) : (
+                    productsList.map((char) => (
+                        <div key={char.id} className="todo-item">
+                            <h3>ID: {char.id}</h3>
+                            <p><strong>Name:</strong> {char.name}</p>
+                            <p><strong>Description:</strong> {char.description}</p>
+                            <p><strong>price:</strong> {char.price}</p>
+                        </div>
+                    ))
+                )}
+            </div>
+        </>
     );
 }
