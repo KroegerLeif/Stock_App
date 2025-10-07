25e08e62922fd407e36a6b34bfd168e69ebe26e9
KroegerLeif <Leif.kr@outlook.com>
2025-10-07 15:39:43 +0200

    Details will be generated from the detail page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,24 +3,16 @@ import {Route, Routes} from "react-router-dom";
 import ViewAllProducts from "./ViewAllProducts.tsx";
 import AddProduct from "./AddProduct.tsx";
 import DetailProductPage from "./component/DetailProductPage.tsx";
-import {useState} from "react";
 
 function App() {
 
-    const [uniqueProduct, setuniqueProduct] = useState({
-        name: "",
-        description: "",
-        price: 0,
-    });
 
   return (
     <>
         <Routes>
             <Route path={"/"} element={<ViewAllProducts/>}/>
             <Route path={"/add"} element={<AddProduct/>}/>
-            <Route path={"/products/:id"} element={<DetailProductPage productName={uniqueProduct.name}
-                                                                      description={uniqueProduct.description}
-                                                                      productPrice={uniqueProduct.price}/>}/>
+            <Route path={"/products/:id"} element={<DetailProductPage/>}/>
         </Routes>
     </>
   )
