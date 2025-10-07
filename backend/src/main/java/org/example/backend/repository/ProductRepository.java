a642b2485283360e73d8d3bdc90f1a80e4bda26e
KroegerLeif <Leif.kr@outlook.com>
2025-10-07 14:56:36 +0200

    makes it able to find a product by Id

--- backend/src/main/java/org/example/backend/repository/ProductRepository.java
+++ backend/src/main/java/org/example/backend/repository/ProductRepository.java
@@ -4,7 +4,9 @@ import org.example.backend.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends MongoRepository<Product, String> {
-
+    Optional<Product> findById(String id);
 }
