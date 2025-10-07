738f17b3af55df5bda907c39e95345bfb9c242de
Der Portugiese <lyapin.daniel@mm-bbs.de>
2025-10-07 11:30:26 +0200

    updated to resolve conflict during rebase

--- backend/src/main/java/org/example/backend/controller/ProductController.java
+++ backend/src/main/java/org/example/backend/controller/ProductController.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import org.example.backend.model.Product;
 import org.example.backend.model.ProductDto;
 import org.example.backend.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,7 +28,13 @@ public class ProductController {
     }
 
     @DeleteMapping("/{id}")
-    public void deleteProductById(@PathVariable String id) {
-        productService.deleteProductById(id);
+    public ResponseEntity<String> deleteProductById(@PathVariable String id) {
+        boolean deletionSuccessful = productService.deleteProductById(id);
+
+        if (deletionSuccessful){
+            return new ResponseEntity<>("Product with id " + id + " was deleted", HttpStatus.OK);
+        } else  {
+            return new ResponseEntity<>("Product with id " + id + " was not found", HttpStatus.NOT_FOUND);
+        }
     }
 }
