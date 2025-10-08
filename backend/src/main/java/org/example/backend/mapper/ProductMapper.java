f3f0a675351e427d0a6ec2accd237de2acfcc721
KroegerLeif <Leif.kr@outlook.com>
2025-10-08 16:36:27 +0200

    Creation of a seprate Mapper Class to get the logik out of the controller

---
+++ backend/src/main/java/org/example/backend/mapper/ProductMapper.java
@@ -0,0 +1,16 @@
+package org.example.backend.mapper;
+
+import org.example.backend.dto.ProductResponse;
+import org.example.backend.model.Product;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductMapper {
+    public ProductResponse mapProductToResponse(Product product) {
+        return new ProductResponse(
+                product.name(),
+                product.description(),
+                product.price()
+        );
+    }
+}
