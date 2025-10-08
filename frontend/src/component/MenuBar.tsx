a053332b253d3d5a3c99416c02228efe753a6a48
KroegerLeif <Leif.kr@outlook.com>
2025-10-08 23:17:06 +0200

    Added class name for menubar

--- frontend/src/component/MenuBar.tsx
+++ frontend/src/component/MenuBar.tsx
@@ -4,14 +4,14 @@ import SearchBar from "./SearchBar.tsx";
 function MenuBar(){
 
     return(
-        <>
+        <div className={"menu-bar"}>
             <nav>
                 <Link to={"/"}>Home</Link>
                 <Link to={"/products/add"}>Add Product</Link>
                 <Link to={"/"}>View All Products</Link>
             </nav>
             <SearchBar/>
-        </>
+        </div>
     )
 }
 
