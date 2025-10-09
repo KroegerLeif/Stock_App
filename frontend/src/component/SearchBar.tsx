fb4d09fe6641e3cf9fd4f86f0ffe914e0559c04d
KroegerLeif <Leif.kr@outlook.com>
2025-10-09 10:23:48 +0200

    Added some search functionality

--- frontend/src/component/SearchBar.tsx
+++ frontend/src/component/SearchBar.tsx
@@ -1,12 +1,40 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
+
 function SearchBar(){
 
-    return(
-        <form className={"search-bar"}>
-            <label>
-                Searchbar
-                <input type={"text"}/>
+    const [searchValue, setSearchValue] = useState("");
+    const [searchResult, setSearchResult] = useState({
+        id: "",
+        name: "",
+        description: "",
+        price: 0
+    });
+    const nav = useNavigate()
+
+    function sumbitSearchForm(){
+        axios.get("/api/products/search/"+searchValue)
+            .then(response => setSearchResult(response.data))
+            .catch(err => console.log(err));
+
+
+    }
+
+    useEffect(() => {
+        setSearchValue("");
+        nav("/products/" + searchResult.id)
+    },[searchResult])
+
+    return (
+        <form className={"search-bar"} onSubmit={sumbitSearchForm}>
+                <input type={"text"}
+                       placeholder={"Search for Product"}
+                    value={searchValue}
+                    onChange={event => setSearchValue(event.target.value)}
+                />
                 <button type={"submit"}>Search</button>
-            </label>
+
         </form>
     )
 }
