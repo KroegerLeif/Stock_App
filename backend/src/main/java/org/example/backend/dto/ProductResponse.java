be7e1974d9740f598eb4074e04d2b3b61bfd6156
malek.b. <malek.boulakbech@gmail.com>
2025-10-14 15:31:04 +0200

    Fix ProductResponse

--- backend/src/main/java/org/example/backend/dto/ProductResponse.java
+++ backend/src/main/java/org/example/backend/dto/ProductResponse.java
@@ -3,5 +3,6 @@ package org.example.backend.dto;
 public record ProductResponse(String id,
                               String name,
                               String description,
+                              int stock,
                               double price) {
 }
