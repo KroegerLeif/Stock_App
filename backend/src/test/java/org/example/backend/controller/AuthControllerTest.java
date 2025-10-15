2978e03421f1ecefec2172574a8ce43e0e0c8b19
malek.b. <malek.boulakbech@gmail.com>
2025-10-15 10:37:16 +0200

    add test

---
+++ backend/src/test/java/org/example/backend/controller/AuthControllerTest.java
@@ -0,0 +1,33 @@
+package org.example.backend.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oidcLogin;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser
+    void getName() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/products")
+                        .with(oidcLogin().userInfoToken(token -> token
+                                .claim("login", "testUser")
+                        ))
+                )
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+}
