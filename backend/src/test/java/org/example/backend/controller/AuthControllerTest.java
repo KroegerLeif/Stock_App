0b0611885da170264458c51df2e3d1c61fae000a
malek.b. <malek.boulakbech@gmail.com>
2025-10-15 11:33:00 +0200

    add test

--- backend/src/test/java/org/example/backend/controller/AuthControllerTest.java
+++ backend/src/test/java/org/example/backend/controller/AuthControllerTest.java
@@ -4,12 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oidcLogin;
 
 @SpringBootTest
@@ -28,6 +31,14 @@ class AuthControllerTest {
                         ))
                 )
                 .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+
+    @Test
+    void testGetMe_withoutUser() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/auth/me"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(""));
     }
 
 }
